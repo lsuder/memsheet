3967a0047833123e091377c2f5d97324914df65a
lsuder <lucas.suder@gmail.com>
2021-05-28 13:50:02 +0200

    Implements SM2.

--- sm2/sm2.js
+++ sm2/sm2.js
@@ -1,15 +1,40 @@
 class SM2 {
-    constructor(quality, repetitions, previousInterval, previousEaseFactor) {
-        this.quality = quality;
+    constructor(repetitions, interval, easeFactor) {
         this.repetitions = repetitions;
-        this.previousInterval = previousInterval;
-        this.previousEaseFactor = previousEaseFactor;
+        this.interval = interval;
+        this.easeFactor = easeFactor;
     }
 
-    static calculate(sm) {
-        const response = new SM2(sm.quality, sm.repetitions, sm.previousInterval, sm.previousEaseFactor);
-        // TODO: implement algorithm.
-        return response;
+    static calculate(prviousSm, quality) {
+        let nextInterval;
+        let nextEaseFactor = prviousSm.easeFactor;
+        let nextRepetitions = prviousSm.repetitions;
+
+        if (quality >= 3) {
+            switch (nextRepetitions) {
+                case 0:
+                    nextInterval = 1;
+                    nextRepetitions = 1;
+                    break;
+                case 1:
+                    nextInterval = 6;
+                    nextRepetitions = 2;
+                    break;
+                default:
+                    nextInterval = Math.round((prviousSm.interval * prviousSm.easeFactor));
+                    nextRepetitions++;
+            }
+
+            nextEaseFactor = prviousSm.easeFactor +
+                (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02));
+        } else {
+            nextInterval = 1;
+            nextRepetitions = 0;
+        }
+
+        if (nextEaseFactor < 1.3) nextEaseFactor = 1.3;
+
+        return new SM2(nextRepetitions, nextInterval, parseFloat(nextEaseFactor.toFixed(4)));
     }
 }
 
--- sm2/sm2.test.js
+++ sm2/sm2.test.js
@@ -1,6 +1,50 @@
 const SM2 = require('./sm2');
 
-test('calculate simple', () => {
-    const previous = new SM2(1, 2, 3, 4);
-    expect(SM2.calculate(previous)).toEqual({ "previousEaseFactor": 4, "previousInterval": 3, "quality": 1, "repetitions": 2 });
+test('Wrong answer: quality = 1, resets interval and repetitions', () => {
+    expect(SM2.calculate({ "easeFactor": 1.3, "interval": 21, "repetitions": 12 },
+        /* quality= */
+        2)).toEqual({ "easeFactor": 1.3, "interval": 1, "repetitions": 0 });
+});
+
+test('Wrong answer: quality = 2, resets interval and repetitions', () => {
+    expect(SM2.calculate({ "easeFactor": 1.3, "interval": 21, "repetitions": 12 },
+            /* quality= */
+            2))
+        .toEqual({ "easeFactor": 1.3, "interval": 1, "repetitions": 0 });
+});
+
+test('Wrong answer does not change easeFactor', () => {
+    expect(SM2.calculate({ "easeFactor": 4, "interval": 21, "repetitions": 12 },
+        /* quality= */
+        1)).toEqual({ "easeFactor": 4, "interval": 1, "repetitions": 0 });
+});
+
+test('Good answer: quality = 3, repetitions = 0, interval = 0', () => {
+    expect(SM2.calculate({ "easeFactor": 1.3, "interval": 0, "repetitions": 0 },
+        /* quality= */
+        3)).toEqual({ "easeFactor": 1.3, "interval": 1, "repetitions": 1 });
+});
+
+test('Good answer: quality = 4, repetitions = 0, interval = 0', () => {
+    expect(SM2.calculate({ "easeFactor": 1.3, "interval": 0, "repetitions": 0 },
+        /* quality= */
+        4)).toEqual({ "easeFactor": 1.3, "interval": 1, "repetitions": 1 });
+});
+
+test('Good answer: quality = 5, repetitions = 0, interval = 0', () => {
+    expect(SM2.calculate({ "easeFactor": 1.3, "interval": 0, "repetitions": 0 },
+        /* quality= */
+        5)).toEqual({ "easeFactor": 1.4, "interval": 1, "repetitions": 1 });
+});
+
+test('Good answer increases interval, repetitions and easeFactor, quality = 5', () => {
+    expect(SM2.calculate({ "easeFactor": 2, "interval": 10, "repetitions": 4 },
+        /* quality= */
+        5)).toEqual({ "easeFactor": 2.1, "interval": 20, "repetitions": 5 });
+});
+
+test('Good answer increases interval and repetitions, quality = 4', () => {
+    expect(SM2.calculate({ "easeFactor": 2, "interval": 10, "repetitions": 4 },
+        /* quality= */
+        4)).toEqual({ "easeFactor": 2, "interval": 20, "repetitions": 5 });
 });
